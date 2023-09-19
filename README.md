5bf0c58c869f2e218d28fb83b0d4dc0036513ccf
Suraj <surajpedasingu@gmail.com>
2023-09-19 23:57:29 +0530

    updated embedding fastapi server

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 ### Instructions to setup NGINX load balancer for multiple fastapi servers
 
-1. create a conda environment and install `nlb_requirements.txt` inside the environment.
-2. install the below commands in 
+1. create a conda environment and install `ems_requirements.txt` inside the environment.
+2. install the below commands inside conda environment 
 ```
-sudo apt-get update
-sudo apt-get install nginx
-```
-3. run the following commands to start nginx reverse proxy load balancing server
-```
-python embedding_model_server.py
-python nginx_load_balancer.py
-[RUN COMMANDS PRINTED IN TERMINAL]
+python embedding_model_server.py --workers 2 --gpus 2 --modelname all-MiniLM-L6-v2
 ```
 ---
--- embedding_model_server.py
+++ embedding_model_server.py
@@ -1,41 +1,72 @@
-from concurrent.futures import ProcessPoolExecutor
-import traceback
-from typing import Callable
+import traceback, argparse
+import uvicorn
+import torch
+from sentence_transformers import SentenceTransformer, util
+from pydantic import BaseModel
+from typing import List, Any
 from fastapi import FastAPI
-from torch import embedding
+from itertools import cycle
+
+# Create a FastAPI app
 app = FastAPI()
+model_name = "all-MiniLM-L6-v2"
+model_workers = {
+    'worker1': SentenceTransformer(model_name, device='cuda:0' if torch.cuda.is_available() else 'cpu')
+}
+round_robin_balancer = cycle(['worker1'])
 
-def create_model():
-    # Your model creation logic
-    model = None
-    return model
-app_model = create_model()
+# Create multiple identical sentence transformer models
+def create_model(num_workers:int, num_gpus:int):
+    global model_name
+    global model_workers
+    global round_robin_balancer
 
+    model_workers = {}
+    model_worker_names = []
+    for worker_num in range(num_workers):
+        try:
+            model = SentenceTransformer(model_name, device=f'cuda:{worker_num%num_gpus}')
+            model_workers[f"worker{worker_num+1}"] = model
+            model.encode(['test sample'])
+            model_worker_names.append(f"worker{worker_num+1}")
+            print(f"{model_name} has been created successfully as worker{worker_num+1} - {model}")
+        except Exception:
+            print(f"{model_name} has not been created as worker{worker_num+1}\n\n{'='*10}{traceback.format_exc()}{'='*10}")
+    print(model_workers)
+    round_robin_balancer = cycle(model_worker_names)
+
+def get_embedding_using_worker(model_worker_name:str, embedding_text: str) -> List[Any]:
+    global model_workers
+    current_model:SentenceTransformer = model_workers[model_worker_name]
+    embedding_vector= list(current_model.encode([embedding_text], normalize_embeddings=True)[0].tolist())
+    return embedding_vector
+
+# define the main 'get_embedding' endpoint
 @app.get("/get_embedding")
-async def get_embedding(payload):
-    global app_model
-    text = str(payload.text)
-    
-    try:
-        # Your embedding logic here
-        # embedding = app_model(text)
-        response = None
-        status = 'success'
-    except:
-        response = traceback.format_exc()
-        status = 'failed'
+async def get_embedding(embedding_text:str):
 
-    return {'status': status, "embedding": response, "text": text}
+    # Get the next worker function using round-robin strategy
+    global round_robin_balancer
+    worker_name = next(round_robin_balancer)
 
-def run_worker(gpu_id):
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000 + gpu_id)
+    #  selected model worker and create embedding for payload text
+    embedding_vector =  get_embedding_using_worker(worker_name,embedding_text)
+    return {
+        "model_name": model_name, 
+        'embedding_text':embedding_text, 
+        "embedding_vector":embedding_vector
+    }
 
+# Run the FastAPI app
 if __name__ == "__main__":
-    num_workers_per_gpu = 2
-    num_gpus = 4
+    parser = argparse.ArgumentParser(description='FastAPI Server with Embedding Model')
+    parser.add_argument('--modelname', type=str, default="all-MiniLM-L6-v2")
+    parser.add_argument('--gpus', type=int, default=1)
+    parser.add_argument('--workers', type=int, default=1)
+    args = parser.parse_args()
 
-    with ProcessPoolExecutor() as executor:
-        for gpu_id in range(num_gpus):
-            for _ in range(num_workers_per_gpu):
-                executor.submit(run_worker, gpu_id)
+    model_name = str(args.modelname)
+    create_model(args.workers, args.gpus)
+
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
